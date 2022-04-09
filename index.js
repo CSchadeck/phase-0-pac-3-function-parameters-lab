b901b772da30da1703ee0b100681ec3defdd0989
Corey Schadeck <corey@schadeck.com>
2022-04-09 18:45:38 -0400

    commit tree 0bd633c7ca740cf552275046d9638b6c3600d235

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name){
+    console.log("Hi my name is ${name}")
+    return ${name}
+}
+intoduction("Corey")
+//function say(greeting, firstName) {
+   // console.log("I was called!");
+   // return `${greeting}, ${firstName}!`;
+  
